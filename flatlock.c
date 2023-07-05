0ef389c28124bab6fa48549a81199297a15a4e0b
lliurex <juanma1980@gmail.com>
2023-07-05 12:39:01 +0200

    recursive create mount path if don't exists yet

--- flatlock.c
+++ flatlock.c
@@ -2,13 +2,44 @@
 This piece of code bind mounts flatpak runtime in the user folder
 effectively blocking any attempts to manage sofware for non privileged users
 */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <libgen.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/mount.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+
+static int mkpath(const char *path, mode_t mode) {
+	char opath[PATH_MAX];
+	char *p;
+	size_t len;
+
+	strncpy(opath, path, sizeof(opath));
+	opath[sizeof(opath) - 1] = '\0';
+	len = strlen(opath);
+	if (len == 0)
+		return (0);
+	else if (opath[len - 1] == '/')
+		opath[len - 1] = '\0';
+	for(p = opath; *p; p++)
+		if (*p == '/') 
+		{
+			*p = '\0';
+			if (access(opath, F_OK))
+				mkdir(opath, mode);
+			*p = '/';
+		}
+	if (access(opath, F_OK)) 
+		mkdir(opath, mode);
+	return(0);
+}
+
 
 int main () {
     struct dirent *pDirent;
@@ -32,7 +63,7 @@ int main () {
 		strcpy(newdir,"/home/");
 		strcat(newdir,tmp);
 		strcat(newdir,rundir);
-		if(mkdir(newdir, 0777) && errno != EEXIST)
+		if(mkpath(newdir, 0777) && errno != EEXIST)
 			printf("Error creating '%s'\n%m\n", newdir); 
 		printf("Mounting '%s'\n%m\n", newdir); 
 		mount(runtimesys,newdir,"",MS_BIND,"");
